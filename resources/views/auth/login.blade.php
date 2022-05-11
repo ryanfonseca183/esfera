9d850c649510b763bb1a725f9127fd3dd7872c2d
ryanfonseca183 <67619131+ryanfonseca183@users.noreply.github.com>
2022-05-11 11:23:36 -0300

    Correções finais

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.auth')
 
 @section('content')
-    <div class="card card-body shadow pt-4 px-5 pb-5 border-0">
+    <div class="card card-body shadow p-4 px-lg-5 pb-lg-5 border-0">
         <h2 class="h3 card-title text-muted mb-2">{{ __('Login') }}</h2>
         <h3 class="h5 card-subtitle mb-4">Realize a autenticação para prosseguir</h3>
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
-            <x-controls.floating-label :label="__('Email Address')" name="email" type="email" autofocus />
+            <x-controls.floating-label label="Endereço de e-mail" name="email" type="email" autofocus />
 
-            <x-controls.floating-label :label="__('Password')" name="password" type="password" />
+            <x-controls.floating-label label="Senha" name="password" type="password" />
 
-            <x-controls.toggle type="checkbox" :label="__('Remember Me')" name="remember" />
+            <x-controls.toggle type="checkbox" label="Me mantenha conectado" name="remember" />
         
             <button class="btn btn-primary mt-3">{{ __('Login') }}</button>
         </form>
--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,27 +1,9 @@
-@props(['title', 'description' => '', 'icon' => ''])
+@props(['title', 'description' => ''])
 
-<div class="row g-4 controls-section" {{ $attributes }}>
+<div class="row g-4" {{ $attributes }}>
     <div class="col-12 col-md-4 col-xxl-3">
-        @if($icon)
-            <div class="row g-3">
-                <div class="col-auto">
-                    <div class="custom-icon-holder icon-large">
-                        <i class="{{$icon}} bi"></i>
-                    </div>
-                </div>
-                <div class="col">
-                    <h3 class="section-title">{{$title}}</h3>
-                    <div class="section-intro">
-                        {!! $description ?? ""!!}
-                    </div>
-                </div>
-            </div>
-        @else
-            <h3 class="section-title">{{$title}}</h3>
-            <div class="section-intro">
-                {!! $description ?? ""!!}
-            </div>
-        @endif
+        <h3>{{$title}}</h3>
+        <div>{!! $description ?? ""!!}</div>
     </div>
     <div class="col-12 col-md-8 col-xxl-6">
         <div class="card card-body border-0 shadow-sm p-4">
